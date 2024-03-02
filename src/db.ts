cd6d4f1ad7c7f0f67451364aaac6e75539603892
Redrogue12 <exgc12@gmail.com>
2024-03-02 10:17:33 -0700

    fix: point to hosted db

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT),
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  port: Number(process.env.DB_PORT || 5432),
 });
 
 export default pool;
