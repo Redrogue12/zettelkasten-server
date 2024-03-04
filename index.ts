f764a37fb7feb736d84a31513191bb22bacda11b
Redrogue12 <exgc12@gmail.com>
2024-03-04 15:09:55 -0700

    add logging with morgan

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import links from "./src/routes/links";
 import users from "./src/routes/users";
 import pool from "./src/db";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
@@ -21,10 +22,27 @@ app.use(
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
+const allowCors = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+};
+
 app.use(express.json()); // for parsing application/json
+
+app.use(morgan("dev"));
 
 // Make pool accessible to our router
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -40,7 +58,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use("/", notes);
 app.use("/", tags);
 app.use("/", links);
-app.use("/", users);
+app.use("/", allowCors, users);
 
 const server = app.listen(3000, () => {
   console.log("Server is running on port 3000");
