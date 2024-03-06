a8632bbe0192ce793b12ded9b79014024c2b9b14
Redrogue12 <exgc12@gmail.com>
2024-03-05 20:31:43 -0700

    get port from env

--- index.ts
+++ index.ts
@@ -60,8 +60,8 @@ app.use("/", tags);
 app.use("/", links);
 app.use("/", users);
 
-const server = app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const server = app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
 });
 
 module.exports = server;
